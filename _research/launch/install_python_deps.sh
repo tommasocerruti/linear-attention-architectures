66a83e2471b19e85dc6142ba5e2165e16629d475
tommasocerruti <tommasocerruti@gmail.com>
2026-04-23 16:20:03 +0200

    Install FLA core for GDN

--- _research/launch/install_python_deps.sh
+++ _research/launch/install_python_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 PKG_DIR=${1:?usage: install_python_deps.sh PKG_DIR}
-MARKER=$PKG_DIR/.deps_installed_v6
+MARKER=$PKG_DIR/.deps_installed_v7
 LOCK=$PKG_DIR/.install.lockdir
 
 mkdir -p "$PKG_DIR"
@@ -37,6 +37,7 @@ fi
 $INSTALL --upgrade --target="$PKG_DIR" transformers wandb datasets sentencepiece einops
 $INSTALL --upgrade --no-deps --target="$PKG_DIR" \
     'causal-conv1d~=1.5' \
+    'fla-core~=0.4.0' \
     'flash-linear-attention~=0.4.0'
 $INSTALL --upgrade --no-deps --target="$PKG_DIR" \
     'git+https://github.com/NVIDIA-NeMo/Emerging-Optimizers.git@v0.2.0'
